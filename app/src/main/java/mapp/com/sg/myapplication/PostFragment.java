2ffecaf5bd8e0da1c02b83be7b35d533bf858fa3
Sacrifice-UD <42086671+Sacrifice-UD@users.noreply.github.com>
2018-08-05 23:04:48 +0800

    added 1/2 of image functions, need to store image in database

--- app/src/main/java/mapp/com/sg/myapplication/PostFragment.java
+++ app/src/main/java/mapp/com/sg/myapplication/PostFragment.java
@@ -3,13 +3,17 @@ package mapp.com.sg.myapplication;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -19,7 +23,7 @@ public class PostFragment extends Fragment {
 
     //widgets
     private ImageView mPostImage;
-    private EditText mTitle, mDescription, mFor, mCountry, mPhoneNumber, mCity, mContactEmail;
+    private EditText mTitle, mDescription, mFor, mCountry, mStateProvince, mPhone, mContactEmail;
     private Button mPost;
     private ProgressBar mProgressBar;
 
@@ -28,12 +32,63 @@ public class PostFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_post, container, false);
+        mPostImage = view.findViewById(R.id.post_image);
+        mTitle = view.findViewById(R.id.input_title);
+        mDescription = view.findViewById(R.id.input_description);
+        mFor = view.findViewById(R.id.input_for);
+        mCountry = view.findViewById(R.id.input_country);
+        mStateProvince = view.findViewById(R.id.input_state_province);
+        mPhone = view.findViewById(R.id.input_phone);
+        mContactEmail = view.findViewById(R.id.input_email);
+        mPost = view.findViewById(R.id.btn_post);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        return view;
     }
+
+    private void init(){
+
+        mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: opening dialog to choose new photo");
+
+            }
+        });
+    }
+
+    private void resetFields(){
+        UniversalImageLoader.setImage("", mPostImage);
+        mTitle.setText("");
+        mDescription.setText("");
+        mFor.setText("");
+        mCountry.setText("");
+        mStateProvince.setText("");
+        mPhone.setText("");
+        mContactEmail.setText("");
+    }
+
+    private void showProgressBar(){
+        mProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideProgressBar(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private boolean isEmpty(String string){
+        return string.equals("");
+    }
+
 
 }
---
+++ app/src/main/java/mapp/com/sg/myapplication/UniversalImageLoader.java
@@ -0,0 +1,48 @@
+package mapp.com.sg.myapplication;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+public class UniversalImageLoader {
+
+    private static final int defaultImage = R.drawable.ic_image_black_24dp;
+    private Context mContext;
+
+    public UniversalImageLoader(Context context) {
+        mContext = context;
+    }
+
+    //When image is loading
+    public ImageLoaderConfiguration getConfig(){
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .showImageOnLoading(defaultImage)
+                .showImageForEmptyUri(defaultImage)
+                .showImageOnFail(defaultImage)
+                .considerExifParams(true)
+                .cacheOnDisk(true).cacheInMemory(true)
+                .cacheOnDisk(true).resetViewBeforeLoading(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(mContext)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .diskCacheSize(100 * 1024 * 1024).build();
+
+        return configuration;
+    }//end of ImageLoaderConfiguration
+
+    //Set the Image
+    public static void setImage(String imgURL, ImageView image){
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.displayImage(imgURL, image);
+    }
+}
