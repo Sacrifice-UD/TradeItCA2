20502447526ec5c518d54ef3c679e5be3bf3358a
Sacrifice-UD <42086671+Sacrifice-UD@users.noreply.github.com>
2018-08-05 23:33:20 +0800

    still working on it, but nearly done

--- app/src/main/java/mapp/com/sg/myapplication/MainActivity.java
+++ app/src/main/java/mapp/com/sg/myapplication/MainActivity.java
@@ -1,13 +1,22 @@
 package mapp.com.sg.myapplication;
 
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private PostFragment postFragment;
     private AccountFragment accountFragment;
 
+    private static final int REQUEST_CODE = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
         mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
         mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
 
+        verifyPermissions();
+
+    }//end of onCreate method
+
+    private void setupViewPager(){
         //inistialising Frame
         homeFragment = new HomeFragment();
         storeFragment = new StoreFragment();
@@ -69,7 +85,36 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }//end of onCreate method
+    }
+
+
+    private void verifyPermissions(){
+        Log.d(TAG, "verifyPermissions: asking user for permissions");
+
+        //asking permissions
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA};
+
+        //if all permissions are allowed, if not ask for the permission
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[1]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[2]) == PackageManager.PERMISSION_GRANTED){
+            setupViewPager();
+        }else{
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    permissions,
+                    REQUEST_CODE);
+        }
+    }//end of verifyPermission method
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        verifyPermissions();
+    }
 
     private void setFragment(Fragment fragment) {
         android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
--- app/src/main/java/mapp/com/sg/myapplication/PostFragment.java
+++ app/src/main/java/mapp/com/sg/myapplication/PostFragment.java
@@ -1,6 +1,8 @@
 package mapp.com.sg.myapplication;
 
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -19,7 +21,7 @@ import static android.content.ContentValues.TAG;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PostFragment extends Fragment {
+public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoSelectedListener {
 
     //widgets
     private ImageView mPostImage;
@@ -27,10 +29,34 @@ public class PostFragment extends Fragment {
     private Button mPost;
     private ProgressBar mProgressBar;
 
+    //variables
+    private Bitmap mSelectedBitmap;
+    private Uri mSelectedUri;
 
     public PostFragment() {
         // Required empty public constructor
     }
+
+    //get from Selecting from gallery
+    @Override
+    public void getImagePath(Uri imagePath) {
+        Log.d(TAG, "getImagePath: setting image to image view");
+        UniversalImageLoader.setImage(imagePath.toString(), mPostImage);
+        //Assign to global variable
+        mSelectedBitmap = null;
+        mSelectedUri = imagePath;
+    }
+
+    //get from taking photo from camera
+    @Override
+    public void getImageBitmap(Bitmap bitmap) {
+        Log.d(TAG, "getImageBitmap: setting image to image view");
+        mPostImage.setImageBitmap(bitmap);
+        //Assign to a global variable
+        mSelectedUri = null;
+        mSelectedBitmap = bitmap;
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,21 +76,34 @@ public class PostFragment extends Fragment {
 
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
+        init();
+
         return view;
     }
 
-    private void init(){
+    private void init() {
 
         mPostImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: opening dialog to choose new photo");
 
+                SelectPhotoDialog dialog = new SelectPhotoDialog();
+                dialog.show(getFragmentManager(), getString(R.string.dialog_select_photo));
+                dialog.setTargetFragment(PostFragment.this, 1);
+            }
+        });
+
+        mPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempting to post.");
+                if (!isEmpty())
             }
         });
     }
 
-    private void resetFields(){
+    private void resetFields() {
         UniversalImageLoader.setImage("", mPostImage);
         mTitle.setText("");
         mDescription.setText("");
@@ -75,20 +114,19 @@ public class PostFragment extends Fragment {
         mContactEmail.setText("");
     }
 
-    private void showProgressBar(){
+    private void showProgressBar() {
         mProgressBar.setVisibility(View.VISIBLE);
 
     }
 
-    private void hideProgressBar(){
-        if(mProgressBar.getVisibility() == View.VISIBLE){
+    private void hideProgressBar() {
+        if (mProgressBar.getVisibility() == View.VISIBLE) {
             mProgressBar.setVisibility(View.INVISIBLE);
         }
     }
 
-    private boolean isEmpty(String string){
+    private boolean isEmpty(String string) {
         return string.equals("");
     }
-
 
 }
