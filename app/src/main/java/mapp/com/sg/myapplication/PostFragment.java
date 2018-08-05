79560f1c657e7c97e4dd854096313f4715da5c02
Sacrifice-UD <42086671+Sacrifice-UD@users.noreply.github.com>
2018-08-06 00:50:41 +0800

    restarting pc

--- app/src/main/java/mapp/com/sg/myapplication/PostFragment.java
+++ app/src/main/java/mapp/com/sg/myapplication/PostFragment.java
@@ -3,7 +3,9 @@ package mapp.com.sg.myapplication;
 
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +16,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import static android.content.ContentValues.TAG;
 
@@ -32,6 +38,7 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
     //variables
     private Bitmap mSelectedBitmap;
     private Uri mSelectedUri;
+    private byte[] mUploadBytes;
 
     public PostFragment() {
         // Required empty public constructor
@@ -94,13 +101,95 @@ public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoS
             }
         });
 
+
         mPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onClick: attempting to post.");
-                if (!isEmpty())
+                Log.d(TAG, "onClick: attempting to post...");
+                //make sure all the fields are not empty before posting it
+                if (!isEmpty(mTitle.getText().toString())
+                        && !isEmpty(mDescription.getText().toString())
+                        && !isEmpty(mFor.getText().toString())
+                        && !isEmpty(mCountry.getText().toString())
+                        && !isEmpty(mStateProvince.getText().toString())
+                        && !isEmpty(mPhone.getText().toString())
+                        && !isEmpty(mContactEmail.getText().toString())) {
+
+                    //we have a bitmap and no Uri
+                    if (mSelectedBitmap != null && mSelectedUri == null) {
+                        uploadNewPhoto(mSelectedBitmap);
+                    }
+                    //we have no bitmap and a uri
+                    else if (mSelectedBitmap == null && mSelectedUri != null) {
+                        uploadNewPhoto(mSelectedUri);
+                    }//end of nested if-else statement
+                } else {
+                    Toast.makeText(getActivity(), "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+                }//end of if-else statement
             }
-        });
+        });//end of onClick listener
+    }//end of init method
+
+    private void uploadNewPhoto(Bitmap bitmap) {
+
+    }
+
+    private void uploadNewPhoto(Uri imagePath) {
+
+    }
+
+    //resizing the image/compressing the image
+    //Doing it on the background task as it can slow down the Main UI
+
+    public class BackgroundImageResize extends AsyncTask<Uri, Integer, byte[]> {
+        Bitmap mBitmap;
+
+        public BackgroundImageResize(Bitmap bitmap) {
+            if (bitmap != null) {
+                this.mBitmap = bitmap;
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getActivity(), "Image is being Compressed", Toast.LENGTH_SHORT).show();
+            showProgressBar();
+        }
+
+        @Override
+        protected byte[] doInBackground(Uri... params) {
+            Log.d(TAG, "doInBackground: started");
+
+            //if it is the Uri, convert it to bitmap and resize it
+            //if it is a bitmap, no need to convert, but resize it
+            if (mBitmap == null) {
+                try {
+                    mBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), params[0]);
+                } catch (IOException e) {
+                    Log.d(TAG, "doInBackground: IOException" + e.getMessage());
+                }
+            }
+            byte[] bytes = null;
+            bytes = getBytesFromBitmap(mBitmap, 100);
+            return bytes;
+
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            super.onPostExecute(bytes);
+            mUploadBytes = bytes;
+            hideProgressBar();
+            //execute the upload task
+        }
+    }
+
+    //convert and compress to the quality
+    public static byte[] getBytesFromBitmap(Bitmap bitmap, int quality) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality,stream);
+        return stream.toByteArray();
     }
 
     private void resetFields() {
