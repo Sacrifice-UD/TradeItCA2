c9a2aff4b91c849da2cc08c74a5b5a0602c765da
lokidoki102 <lokidoki102@users.noreply.github.com>
2018-08-08 22:39:46 +0800

    testing

--- app/src/main/java/mapp/com/sg/myapplication/RecyclerViewAdapter.java
+++ app/src/main/java/mapp/com/sg/myapplication/RecyclerViewAdapter.java
@@ -1,4 +1,71 @@
 package mapp.com.sg.myapplication;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+//    debug
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<String> mImageNames, ArrayList<String> mImages) {
+        this.mImageNames = mImageNames;
+        this.mImages = mImages;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        //add glide
+        holder.imageName.setText(mImageNames.get(position));
+
+        //add onclick listener
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on " +mImageNames.get(position));
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        SquareImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
 }
