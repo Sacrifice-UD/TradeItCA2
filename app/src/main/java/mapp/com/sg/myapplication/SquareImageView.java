35e92266eb84af06514cfff607356109c77bb529
lokidoki102 <lokidoki102@users.noreply.github.com>
2018-08-09 14:24:32 +0800

    minor changes to squareimage

--- app/src/main/java/mapp/com/sg/myapplication/SquareImageView.java
+++ app/src/main/java/mapp/com/sg/myapplication/SquareImageView.java
@@ -2,18 +2,17 @@ package mapp.com.sg.myapplication;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-public class SquareImageView extends ImageView{
+public class SquareImageView extends AppCompatImageView {
     public SquareImageView(Context context) {
         super(context);
     }
-
     public SquareImageView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
-
     public SquareImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
